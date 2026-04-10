41b2b144ab31bc1f82543a5aec79b7717e8d0b5d
jiangkai <849433012@qq.com>
2026-04-10 15:38:01 +0800

    更改后端调用大模型的接口

--- server/router/llm_ask.py
+++ server/router/llm_ask.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from openai import AsyncOpenAI
-from config import config
 
 llm_ask_router = APIRouter(prefix="/llm")
 
@@ -10,6 +9,10 @@ class LLMAsk(BaseModel):
     book_name: str
     question: str
     image_base64: str
+    image_content:str
+    api_key: str
+    base_url: str
+    model: str
 
 
 @llm_ask_router.post("/llm_ask")
@@ -19,21 +22,22 @@ async def llm_ask(llm_ask: LLMAsk):
     你是一个专业的计算机领域的专家，现在用户正在阅读一本名为{llm_ask.book_name}的书，用户现在遇到了一个问题，请你根据书中的内容以及用户的问题，给出详细的解答。
     用户问题是：
     {llm_ask.question}
+    提问的书本截图的内容是：
+    {llm_ask.image_content}
     请根据书中的内容，给出详细的解答。
     """
 
     client = AsyncOpenAI(
-        # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
-        api_key=config.api_key,
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+        api_key=llm_ask.api_key,
+        base_url=llm_ask.base_url,
     )
     completion = await client.chat.completions.create(
-        model="qwen-vl-max-latest",
+        model=llm_ask.model,
         messages=[
             {
                 "role": "system",
                 "content": [
-                    {"type": "text", "text": "You are a Professional computer experts."}
+                    {"type": "text", "text": "You are a Professional experts."}
                 ],
             },
             {
